c8f21653286e4b09fe9542f2792772cbc7936a13
Cshan9 <shiva_mahan@hotmail.com>
2015-10-28 11:54:37 -0400

    view order controller added for future template

---
+++ js/controllers/ViewOrderCtrl.js
@@ -0,0 +1,16 @@
+app.controller('ViewOrderCtrl', ViewOrderCtrl);
+
+function ViewOrderCtrl(OrderService,$location,$routeParams){
+    this.location = $location;
+    this.orderId = $routeParams.orderId;
+    this.orderService = OrderService;
+    this.getOrder();
+}
+
+
+ViewOrderCtrl.prototype.getOrder = function(){
+	this.order = this.orderService.getOrder();
+}
+
+
+
