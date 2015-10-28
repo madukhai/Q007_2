dabc1c67cca0862bc94af8afe994e1001265eca1
madukhai <brainstation@BrainStations-MacBook-Air-3.local>
2015-10-28 17:10:11 -0400

    orders

--- js/controllers/ViewOrderCtrl.js
+++ js/controllers/ViewOrderCtrl.js
@@ -1,9 +1,10 @@
 app.controller('ViewOrderCtrl', ViewOrderCtrl);
 
-function ViewOrderCtrl(OrderService,$location,$routeParams){
+function ViewOrderCtrl(OrderService,$location,$routeParams,order){
     this.location = $location;
     this.orderId = $routeParams.orderId;
     this.orderService = OrderService;
+    this.order = order;
     // this.getOrder();
 }
 
