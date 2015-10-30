40da967337dee33fe845855e1bd9b751292b1c7f
madukhai <brainstation@BrainStations-MacBook-Air-3.local>
2015-10-30 11:36:10 -0400

    fix deleting

--- js/services/ApiService.js
+++ js/services/ApiService.js
@@ -8,7 +8,7 @@ angular.module('ShopApp').service('api', ApiService);
 
 //constants
 var BASE_URL = 'http://174.129.248.23/brainstation/shop';
-var HEADERS = {headers : {'Content-Type': 'application/x-www-form-urlencoded'}};
+var HEADERS = {headers : {'Content-Type': 'application/x-www-form-urlencoded','Authorization':'HAFS7NBAF'}};
 
 
 ApiService.prototype.request = function(endpoint,data,method) {
